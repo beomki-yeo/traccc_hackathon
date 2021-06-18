9230dc35f9ba891e63679208b8835dd8f86df161
beomki yeo <beomki.yeo@gmail.com>
2021-06-17 23:53:02 -0700

    finish grid size change

--- cuda/src/algorithms/seeding/seed_selecting.cu
+++ cuda/src/algorithms/seeding/seed_selecting.cu
@@ -60,8 +60,12 @@ void seed_selecting(const seedfilter_config& filter_config,
     auto triplet_container_view = get_data(triplet_container, resource);
     auto seed_container_view = get_data(seed_container, resource);
 
-    unsigned int num_threads = WARP_SIZE * 1;
-    unsigned int num_blocks = internal_sp_view.headers.m_size;
+    unsigned int num_threads = WARP_SIZE * 2;
+    unsigned int num_blocks = 0;
+    for (size_t i=0; i<internal_sp_view.headers.m_size; ++i){
+	num_blocks += triplet_counter_container.headers[i] / num_threads +1;
+    }
+    
     unsigned int sh_mem =
         sizeof(triplet) * num_threads * filter_config.max_triplets_per_spM;
 
@@ -90,149 +94,152 @@ __global__ void seed_selecting_kernel(
         {triplet_view.headers, triplet_view.items});
     device_seed_container seed_device({seed_view.headers, seed_view.items});
 
-    auto bin_info = internal_sp_device.headers.at(blockIdx.x);
-    auto internal_sp_per_bin = internal_sp_device.items.at(blockIdx.x);
+    unsigned int n_bins = internal_sp_device.headers.size();
+    unsigned int bin_idx = 0;
+    unsigned int ref_block_idx = 0;
+
+    cuda_helper::get_bin_idx(n_bins,
+			     triplet_counter_device,
+			     bin_idx,
+			     ref_block_idx);
+    
+    auto internal_sp_per_bin = internal_sp_device.items.at(bin_idx);
     auto& num_compat_spM_per_bin =
-        doublet_counter_device.headers.at(blockIdx.x);
-    auto doublet_counter_per_bin = doublet_counter_device.items.at(blockIdx.x);
+        doublet_counter_device.headers.at(bin_idx);
+    auto doublet_counter_per_bin = doublet_counter_device.items.at(bin_idx);
 
-    auto& num_compat_mb_per_bin = triplet_counter_device.headers.at(blockIdx.x);
-    auto triplet_counter_per_bin = triplet_counter_device.items.at(blockIdx.x);
+    auto& num_compat_mb_per_bin = triplet_counter_device.headers.at(bin_idx);
+    auto triplet_counter_per_bin = triplet_counter_device.items.at(bin_idx);
 
-    auto& num_triplets_per_bin = triplet_device.headers.at(blockIdx.x);
-    auto triplets_per_bin = triplet_device.items.at(blockIdx.x);
+    auto& num_triplets_per_bin = triplet_device.headers.at(bin_idx);
+    auto triplets_per_bin = triplet_device.items.at(bin_idx);
 
     auto& num_seeds = seed_device.headers.at(0);
     auto seeds = seed_device.items.at(0);
 
-    size_t n_iter = num_compat_spM_per_bin / blockDim.x + 1;
-
-    // zero initialization
     extern __shared__ triplet triplets_per_spM[];
     __syncthreads();
 
-    for (size_t i_it = 0; i_it < n_iter; ++i_it) {
-        auto gid = i_it * blockDim.x + threadIdx.x;
-
-        if (gid >= num_compat_spM_per_bin) {
-            continue;
-        }
-
-        auto& spM_loc = doublet_counter_per_bin[gid].spM;
-        auto& spM_idx = spM_loc.sp_idx;
-        auto& spM = internal_sp_per_bin[spM_idx];
-
-        if (spM_idx >= doublet_counter_per_bin.size()) {
-            continue;
-        }
-
-        size_t n_triplets_per_spM = 0;
-
-        size_t stride = threadIdx.x * filter_config.max_triplets_per_spM;
-
-        for (size_t i = 0; i < num_triplets_per_bin; ++i) {
-            auto& aTriplet = triplets_per_bin[i];
-            auto& spB_loc = aTriplet.sp1;
-            auto& spT_loc = aTriplet.sp3;
-            auto& spB =
-                internal_sp_device.items[spB_loc.bin_idx][spB_loc.sp_idx];
-            auto& spT =
-                internal_sp_device.items[spT_loc.bin_idx][spT_loc.sp_idx];
-
-            if (spM_loc == aTriplet.sp2) {
-                seed_selecting_helper::seed_weight(filter_config, spB, spT,
-                                                   aTriplet.weight);
-
-                if (!seed_selecting_helper::single_seed_cut(filter_config, spB,
-                                                            aTriplet.weight)) {
-                    continue;
-                }
-
-                if (n_triplets_per_spM >= filter_config.max_triplets_per_spM) {
-                    int begin_idx = stride;
-                    int end_idx = stride + filter_config.max_triplets_per_spM;
-
-                    // Note: min_index method gives a result different
-                    //       from sorting method when there are the cases where
-                    //       weight & z_vertex are same.
-                    //
-                    //       So min_index method reduces seed matching ratio
-                    //       since the cpu version is using sorting method.
-                    //
-                    //       But that doesn't mean min_index method
-                    //       is wrong of course
-                    //
-                    //       Let's not be so obsessed about achieving
-                    //       perfectly same result :))))))))
-		    
-                    int min_index =
-                        std::min_element(triplets_per_spM + begin_idx,
-                                         triplets_per_spM + end_idx,
-                                         triplet_weight_compare) -
-                        triplets_per_spM;
-
-                    auto& min_weight = triplets_per_spM[min_index].weight;
-
-                    if (aTriplet.weight > min_weight) {
-                        triplets_per_spM[min_index] = aTriplet;
-                    }
-		    
-                    // (deprecated) sorting method -> good for seed matching ratio but slow
-                    /*
-                    thrust::sort(thrust::seq,
-                                 triplets_per_spM+begin_idx,
-                                 triplets_per_spM+end_idx,
-                                 triplet_weight_descending());
-
-                    if (aTriplet.weight >= triplets_per_spM[end_idx-1].weight){
-                        triplets_per_spM[end_idx-1] = aTriplet;
-                    }
-                    */
-                }
-
-                else if (n_triplets_per_spM <
-                         filter_config.max_triplets_per_spM) {
-                    triplets_per_spM[stride + n_triplets_per_spM] = aTriplet;
-                    n_triplets_per_spM++;
-                }
-            }
-        }
-
-        // sort the triplets per spM
-        // sequential version of thrust sorting algorithm is used
-        thrust::sort(thrust::seq, triplets_per_spM + stride,
-                     triplets_per_spM + stride + n_triplets_per_spM,
-                     triplet_weight_descending());
-
-        size_t n_seeds_per_spM = 0;
-
-        for (size_t i = stride; i < stride + n_triplets_per_spM; ++i) {
-            auto& aTriplet = triplets_per_spM[i];
-            auto& spB_loc = aTriplet.sp1;
-            auto& spT_loc = aTriplet.sp3;
-            auto& spB =
-                internal_sp_device.items[spB_loc.bin_idx][spB_loc.sp_idx];
-            auto& spT =
-                internal_sp_device.items[spT_loc.bin_idx][spT_loc.sp_idx];
-
-            if (n_seeds_per_spM >= filter_config.maxSeedsPerSpM + 1) {
-                break;
-            }
-
-            if (seed_selecting_helper::cut_per_middle_sp(filter_config, spB,
-                                                         aTriplet.weight) ||
-                n_seeds_per_spM == 0) {
-                auto pos = atomicAdd(&num_seeds, 1);
-
-                if (pos >= seeds.size()) {
-                    break;
-                }
-                n_seeds_per_spM++;
-
-                seeds[pos] = seed({spB.m_sp, spM.m_sp, spT.m_sp,
-                                   aTriplet.weight, aTriplet.z_vertex});
-            }
-        }
+    auto gid = (blockIdx.x - ref_block_idx) * blockDim.x + threadIdx.x;
+    
+    if (gid >= num_compat_spM_per_bin) {
+	return;
+    }
+    
+    auto& spM_loc = doublet_counter_per_bin[gid].spM;
+    auto& spM_idx = spM_loc.sp_idx;
+    auto& spM = internal_sp_per_bin[spM_idx];
+    
+    if (spM_idx >= doublet_counter_per_bin.size()) {
+	return;
+    }
+    
+    size_t n_triplets_per_spM = 0;
+    
+    size_t stride = threadIdx.x * filter_config.max_triplets_per_spM;
+    
+    for (size_t i = 0; i < num_triplets_per_bin; ++i) {
+	auto& aTriplet = triplets_per_bin[i];
+	auto& spB_loc = aTriplet.sp1;
+	auto& spT_loc = aTriplet.sp3;
+	auto& spB =
+	    internal_sp_device.items[spB_loc.bin_idx][spB_loc.sp_idx];
+	auto& spT =
+	    internal_sp_device.items[spT_loc.bin_idx][spT_loc.sp_idx];
+	
+	if (spM_loc == aTriplet.sp2) {
+	    seed_selecting_helper::seed_weight(filter_config, spB, spT,
+					       aTriplet.weight);
+	    
+	    if (!seed_selecting_helper::single_seed_cut(filter_config, spB,
+							aTriplet.weight)) {
+		continue;
+	    }
+	    
+	    if (n_triplets_per_spM >= filter_config.max_triplets_per_spM) {
+		int begin_idx = stride;
+		int end_idx = stride + filter_config.max_triplets_per_spM;
+		
+		// Note: min_index method gives a result different
+		//       from sorting method when there are the cases where
+		//       weight & z_vertex are same.
+		//
+		//       So min_index method reduces seed matching ratio
+		//       since the cpu version is using sorting method.
+		//
+		//       But that doesn't mean min_index method
+		//       is wrong of course
+		//
+		//       Let's not be so obsessed about achieving
+		//       perfectly same result :))))))))
+		
+		int min_index =
+		    std::min_element(triplets_per_spM + begin_idx,
+				     triplets_per_spM + end_idx,
+				     triplet_weight_compare) -
+		    triplets_per_spM;
+		
+		auto& min_weight = triplets_per_spM[min_index].weight;
+		
+		if (aTriplet.weight > min_weight) {
+		    triplets_per_spM[min_index] = aTriplet;
+		}
+		
+		// (deprecated) sorting method -> good for seed matching ratio but slow
+		/*
+		  thrust::sort(thrust::seq,
+		  triplets_per_spM+begin_idx,
+		  triplets_per_spM+end_idx,
+		  triplet_weight_descending());
+		  
+		  if (aTriplet.weight >= triplets_per_spM[end_idx-1].weight){
+		  triplets_per_spM[end_idx-1] = aTriplet;
+		  }
+		*/
+	    }
+	    
+	    else if (n_triplets_per_spM <
+		     filter_config.max_triplets_per_spM) {
+		triplets_per_spM[stride + n_triplets_per_spM] = aTriplet;
+		n_triplets_per_spM++;
+	    }
+	}
+    }
+    
+    // sort the triplets per spM
+    // sequential version of thrust sorting algorithm is used
+    thrust::sort(thrust::seq, triplets_per_spM + stride,
+		 triplets_per_spM + stride + n_triplets_per_spM,
+		 triplet_weight_descending());
+    
+    size_t n_seeds_per_spM = 0;
+    
+    for (size_t i = stride; i < stride + n_triplets_per_spM; ++i) {
+	auto& aTriplet = triplets_per_spM[i];
+	auto& spB_loc = aTriplet.sp1;
+	auto& spT_loc = aTriplet.sp3;
+	auto& spB =
+	    internal_sp_device.items[spB_loc.bin_idx][spB_loc.sp_idx];
+	auto& spT =
+	    internal_sp_device.items[spT_loc.bin_idx][spT_loc.sp_idx];
+	
+	if (n_seeds_per_spM >= filter_config.maxSeedsPerSpM + 1) {
+	    break;
+	}
+	
+	if (seed_selecting_helper::cut_per_middle_sp(filter_config, spB,
+						     aTriplet.weight) ||
+	    n_seeds_per_spM == 0) {
+	    auto pos = atomicAdd(&num_seeds, 1);
+	    
+	    if (pos >= seeds.size()) {
+		break;
+	    }
+	    n_seeds_per_spM++;
+	    
+	    seeds[pos] = seed({spB.m_sp, spM.m_sp, spT.m_sp,
+			       aTriplet.weight, aTriplet.z_vertex});
+	}
     }
 }
 
--- cuda/src/algorithms/seeding/weight_updating.cu
+++ cuda/src/algorithms/seeding/weight_updating.cu
@@ -6,6 +6,7 @@
  */
 
 #include <cuda/algorithms/seeding/weight_updating.cuh>
+#include <cuda/utils/cuda_helper.cuh>
 #include <cuda/utils/definitions.hpp>
 
 namespace traccc {
@@ -22,16 +23,20 @@ void weight_updating(const seedfilter_config& filter_config,
                      host_triplet_counter_container& triplet_counter_container,
                      host_triplet_container& triplet_container,
                      vecmem::memory_resource* resource) {
-    auto internal_sp_data = get_data(internal_sp_container, resource);
+    auto internal_sp_view = get_data(internal_sp_container, resource);
     auto triplet_counter_view = get_data(triplet_counter_container, resource);
     auto triplet_view = get_data(triplet_container, resource);
 
-    unsigned int num_threads = WARP_SIZE * 8;
-    unsigned int num_blocks = internal_sp_data.headers.m_size;
+    unsigned int num_threads = WARP_SIZE * 2;
+    unsigned int num_blocks = 0;
+    for (size_t i=0; i<internal_sp_view.headers.m_size; ++i){
+	num_blocks += triplet_container.headers[i] / num_threads +1;
+    }
+    
     unsigned int sh_mem = sizeof(float) * filter_config.compatSeedLimit;
 
     weight_updating_kernel<<<num_blocks, num_threads, sh_mem>>>(
-        filter_config, internal_sp_data, triplet_counter_view, triplet_view);
+								filter_config, internal_sp_view, triplet_counter_view, triplet_view);
 
     CUDA_ERROR_CHECK(cudaGetLastError());
     CUDA_ERROR_CHECK(cudaDeviceSynchronize());
@@ -50,125 +55,129 @@ __global__ void weight_updating_kernel(
     device_triplet_container triplet_device(
         {triplet_view.headers, triplet_view.items});
 
-    auto bin_info = internal_sp_device.headers.at(blockIdx.x);
-    auto internal_sp_per_bin = internal_sp_device.items.at(blockIdx.x);
+    unsigned int n_bins = internal_sp_device.headers.size();
+    unsigned int bin_idx = 0;
+    unsigned int ref_block_idx = 0;
 
-    auto& num_compat_mb_per_bin = triplet_counter_device.headers.at(blockIdx.x);
-    auto triplet_counter_per_bin = triplet_counter_device.items.at(blockIdx.x);
+    cuda_helper::get_bin_idx(n_bins,
+			     triplet_device,
+			     bin_idx,
+			     ref_block_idx);
+    
+    auto internal_sp_per_bin = internal_sp_device.items.at(bin_idx);
 
-    auto& num_triplets_per_bin = triplet_device.headers.at(blockIdx.x);
-    auto triplets_per_bin = triplet_device.items.at(blockIdx.x);
+    auto& num_compat_mb_per_bin = triplet_counter_device.headers.at(bin_idx);
+    auto triplet_counter_per_bin = triplet_counter_device.items.at(bin_idx);
 
-    size_t n_iter = num_triplets_per_bin / blockDim.x + 1;
+    auto& num_triplets_per_bin = triplet_device.headers.at(bin_idx);
+    auto triplets_per_bin = triplet_device.items.at(bin_idx);
 
     // zero initialization
     extern __shared__ float compat_seedR[];
     __syncthreads();
 
-    for (size_t i_it = 0; i_it < n_iter; ++i_it) {
-        auto tr_idx = i_it * blockDim.x + threadIdx.x;
-        auto& triplet = triplets_per_bin[tr_idx];
-        auto& spB_idx = triplet.sp1;
-        auto& spM_idx = triplet.sp2;
-        auto& spT_idx = triplet.sp3;
+    auto tr_idx = (blockIdx.x - ref_block_idx) * blockDim.x + threadIdx.x;
+    auto& triplet = triplets_per_bin[tr_idx];
+    auto& spB_idx = triplet.sp1;
+    auto& spM_idx = triplet.sp2;
+    auto& spT_idx = triplet.sp3;
+    
+    if (tr_idx >= num_triplets_per_bin) {
+	return;
+    }
+    
+    size_t start_idx = 0;
+    size_t end_idx = 0;
+    
+    for (auto triplet_counter : triplet_counter_per_bin) {
+	end_idx += triplet_counter.n_triplets;
+	
+	if (triplet_counter.mid_bot_doublet.sp1 == spM_idx &&
+	    triplet_counter.mid_bot_doublet.sp2 == spB_idx) {
+	    break;
+	}
+	
+	start_idx += triplet_counter.n_triplets;
+    }
+    
+    if (end_idx >= triplets_per_bin.size()) {
+	end_idx = fmin(triplets_per_bin.size(), end_idx);
+    }
+    
+    if (start_idx >= triplets_per_bin.size()) {
+	return;
+    }
+    
+    auto& current_spT =
+	internal_sp_device.items[spT_idx.bin_idx][spT_idx.sp_idx];
 
-        if (tr_idx >= num_triplets_per_bin) {
-            continue;
-        }
-
-        size_t start_idx = 0;
-        size_t end_idx = 0;
-
-        for (auto triplet_counter : triplet_counter_per_bin) {
-            end_idx += triplet_counter.n_triplets;
-
-            if (triplet_counter.mid_bot_doublet.sp1 == spM_idx &&
-                triplet_counter.mid_bot_doublet.sp2 == spB_idx) {
-                break;
-            }
-
-            start_idx += triplet_counter.n_triplets;
-        }
-
-        if (end_idx >= triplets_per_bin.size()) {
-            end_idx = fmin(triplets_per_bin.size(), end_idx);
-        }
-
-        if (start_idx >= triplets_per_bin.size()) {
-            continue;
-        }
-
-        auto& current_spT =
-            internal_sp_device.items[spT_idx.bin_idx][spT_idx.sp_idx];
-
-        float currentTop_r = current_spT.radius();
-
-        // if two compatible seeds with high distance in r are found, compatible
-        // seeds span 5 layers
-        // -> very good seed
-        float lowerLimitCurv =
-            triplet.curvature - filter_config.deltaInvHelixDiameter;
-        float upperLimitCurv =
-            triplet.curvature + filter_config.deltaInvHelixDiameter;
-        int num_compat_seedR = 0;
-
-        // iterate over triplets
-        for (auto tr_it = triplets_per_bin.begin() + start_idx;
-             tr_it != triplets_per_bin.begin() + end_idx; tr_it++) {
-            if (triplet == *tr_it) {
-                continue;
-            }
-
-            auto& other_triplet = *tr_it;
-            auto other_spT_idx = (*tr_it).sp3;
-            auto other_spT =
-                internal_sp_device
-                    .items[other_spT_idx.bin_idx][other_spT_idx.sp_idx];
-
-            // compared top SP should have at least deltaRMin distance
-            float otherTop_r = other_spT.radius();
-            float deltaR = currentTop_r - otherTop_r;
-            if (std::abs(deltaR) < filter_config.deltaRMin) {
-                continue;
-            }
-
-            // curvature difference within limits?
-            // TODO: how much slower than sorting all vectors by curvature
-            // and breaking out of loop? i.e. is vector size large (e.g. in
-            // jets?)
-            if (other_triplet.curvature < lowerLimitCurv) {
-                continue;
-            }
-            if (other_triplet.curvature > upperLimitCurv) {
-                continue;
-            }
-
-            bool newCompSeed = true;
-
-            for (size_t i_s = 0; i_s < num_compat_seedR; ++i_s) {
-                float previousDiameter = compat_seedR[i_s];
-
-                // original ATLAS code uses higher min distance for 2nd found
-                // compatible seed (20mm instead of 5mm) add new compatible seed
-                // only if distance larger than rmin to all other compatible
-                // seeds
-                if (std::abs(previousDiameter - otherTop_r) <
-                    filter_config.deltaRMin) {
-                    newCompSeed = false;
-                    break;
-                }
-            }
-
-            if (newCompSeed) {
-                compat_seedR[num_compat_seedR] = otherTop_r;
-                triplet.weight += filter_config.compatSeedWeight;
-                num_compat_seedR++;
-            }
-
-            if (num_compat_seedR >= filter_config.compatSeedLimit) {
-                break;
-            }
-        }
+    float currentTop_r = current_spT.radius();
+    
+    // if two compatible seeds with high distance in r are found, compatible
+    // seeds span 5 layers
+    // -> very good seed
+    float lowerLimitCurv =
+	triplet.curvature - filter_config.deltaInvHelixDiameter;
+    float upperLimitCurv =
+	triplet.curvature + filter_config.deltaInvHelixDiameter;
+    int num_compat_seedR = 0;
+    
+    // iterate over triplets
+    for (auto tr_it = triplets_per_bin.begin() + start_idx;
+	 tr_it != triplets_per_bin.begin() + end_idx; tr_it++) {
+	if (triplet == *tr_it) {
+	    continue;
+	}
+	
+	auto& other_triplet = *tr_it;
+	auto other_spT_idx = (*tr_it).sp3;
+	auto other_spT =
+	    internal_sp_device
+	    .items[other_spT_idx.bin_idx][other_spT_idx.sp_idx];
+	
+	// compared top SP should have at least deltaRMin distance
+	float otherTop_r = other_spT.radius();
+	float deltaR = currentTop_r - otherTop_r;
+	if (std::abs(deltaR) < filter_config.deltaRMin) {
+	    continue;
+	}
+	
+	// curvature difference within limits?
+	// TODO: how much slower than sorting all vectors by curvature
+	// and breaking out of loop? i.e. is vector size large (e.g. in
+	// jets?)
+	if (other_triplet.curvature < lowerLimitCurv) {
+	    continue;
+	}
+	if (other_triplet.curvature > upperLimitCurv) {
+	    continue;
+	}
+	
+	bool newCompSeed = true;
+	
+	for (size_t i_s = 0; i_s < num_compat_seedR; ++i_s) {
+	    float previousDiameter = compat_seedR[i_s];
+	    
+	    // original ATLAS code uses higher min distance for 2nd found
+	    // compatible seed (20mm instead of 5mm) add new compatible seed
+	    // only if distance larger than rmin to all other compatible
+	    // seeds
+	    if (std::abs(previousDiameter - otherTop_r) <
+		filter_config.deltaRMin) {
+		newCompSeed = false;
+		break;
+	    }
+	}
+	
+	if (newCompSeed) {
+	    compat_seedR[num_compat_seedR] = otherTop_r;
+	    triplet.weight += filter_config.compatSeedWeight;
+	    num_compat_seedR++;
+	}
+	
+	if (num_compat_seedR >= filter_config.compatSeedLimit) {
+	    break;
+	}
     }
 }
 
