5b97edfc3c602c800346e8ba891cccf1341b8d6f
beomki yeo <beomki.yeo@gmail.com>
2021-07-16 13:10:06 -0700

    fix matrix multiplication test

--- tests/cuda/algebra/matmul_kernel.cu
+++ tests/cuda/algebra/matmul_kernel.cu
@@ -8,55 +8,35 @@
 #include <cuda_runtime.h>
 #include "matmul_kernel.cuh"
 
+// kernel declaration
+template < typename matrix_t >
 __global__ void matmul_kernel(
-    vecmem::data::vector_view< Acts::BoundSymMatrix > A_view,
-    vecmem::data::vector_view< Acts::BoundSymMatrix > B_view,
-    vecmem::data::vector_view< Acts::BoundSymMatrix > C_view);
+    vecmem::data::vector_view< matrix_t > A_view,
+    vecmem::data::vector_view< matrix_t > B_view,
+    vecmem::data::vector_view< matrix_t > C_view);
 
-__global__ void matmul_kernel(
-    vecmem::data::vector_view< Acts::FreeSymMatrix > A_view,
-    vecmem::data::vector_view< Acts::FreeSymMatrix > B_view,
-    vecmem::data::vector_view< Acts::FreeSymMatrix > C_view);
-
-void matmul(int n_matrix,
+// explicit instantiation of matmul function
+template 
+void matmul<Acts::BoundSymMatrix>(int n_matrix,
 	    vecmem::data::vector_view< Acts::BoundSymMatrix > A_view,
 	    vecmem::data::vector_view< Acts::BoundSymMatrix > B_view,
 	    vecmem::data::vector_view< Acts::BoundSymMatrix > C_view,
-	    float& time){
+	    float& elapsed);
 
-    unsigned int num_threads = WARP_SIZE*2;
-    unsigned int num_blocks = n_matrix/num_threads + 1;
-
-    //--TIME--------------------------------------
-    float elapsed=0;
-    cudaEvent_t start, stop;    
-    CUDA_ERROR_CHECK(cudaEventCreate(&start));
-    CUDA_ERROR_CHECK(cudaEventCreate(&stop));
-    CUDA_ERROR_CHECK(cudaEventRecord(start, 0));
-    //--------------------------------------------
-    
-    matmul_kernel<<< num_blocks, num_threads >>>(A_view, B_view, C_view);
-
-    // cuda error check    
-    CUDA_ERROR_CHECK(cudaGetLastError());
-    CUDA_ERROR_CHECK(cudaDeviceSynchronize());
-    
-    //--TIME--------------------------------------
-    CUDA_ERROR_CHECK(cudaEventRecord(stop, 0));
-    CUDA_ERROR_CHECK(cudaEventSynchronize (stop) );    
-    CUDA_ERROR_CHECK(cudaEventElapsedTime(&elapsed, start, stop) );    
-    CUDA_ERROR_CHECK(cudaEventDestroy(start));
-    CUDA_ERROR_CHECK(cudaEventDestroy(stop));
-    elapsed*=0.001; // ms->sec
-    time += elapsed;
-    //--------------------------------------------
-    
-}
-
-void matmul(int n_matrix,
+// explicit instantiation of matmul function
+template 
+void matmul<Acts::FreeSymMatrix>(int n_matrix,
 	    vecmem::data::vector_view< Acts::FreeSymMatrix > A_view,
 	    vecmem::data::vector_view< Acts::FreeSymMatrix > B_view,
 	    vecmem::data::vector_view< Acts::FreeSymMatrix > C_view,
+	    float& elapsed);
+
+// Definition of matmul function
+template < typename matrix_t >
+void matmul(int n_matrix,
+	    vecmem::data::vector_view< matrix_t > A_view,
+	    vecmem::data::vector_view< matrix_t > B_view,
+	    vecmem::data::vector_view< matrix_t > C_view,
 	    float& time){
 
     unsigned int num_threads = WARP_SIZE*2;
@@ -72,6 +52,10 @@ void matmul(int n_matrix,
     
     matmul_kernel<<< num_blocks, num_threads >>>(A_view, B_view, C_view);
 
+    // cuda error check    
+    CUDA_ERROR_CHECK(cudaGetLastError());
+    CUDA_ERROR_CHECK(cudaDeviceSynchronize());
+    
     //--TIME--------------------------------------
     CUDA_ERROR_CHECK(cudaEventRecord(stop, 0));
     CUDA_ERROR_CHECK(cudaEventSynchronize (stop) );    
@@ -82,44 +66,25 @@ void matmul(int n_matrix,
     time += elapsed;
     //--------------------------------------------
     
-    // cuda error check    
-    CUDA_ERROR_CHECK(cudaGetLastError());
-    CUDA_ERROR_CHECK(cudaDeviceSynchronize());
 }
 
-
+// kernel definition
+template < typename matrix_t >
 __global__ void matmul_kernel(
-    vecmem::data::vector_view< Acts::BoundSymMatrix > A_view,
-    vecmem::data::vector_view< Acts::BoundSymMatrix > B_view,
-    vecmem::data::vector_view< Acts::BoundSymMatrix > C_view){
+    vecmem::data::vector_view< matrix_t > A_view,
+    vecmem::data::vector_view< matrix_t > B_view,
+    vecmem::data::vector_view< matrix_t > C_view){
+
+    int gid = blockIdx.x*blockDim.x + threadIdx.x;
+
+    vecmem::device_vector< matrix_t > A_dev(A_view);
+    vecmem::device_vector< matrix_t > B_dev(B_view);
+    vecmem::device_vector< matrix_t > C_dev(C_view);
+
+    if (gid >= A_dev.size()){
+	return;
+    }
+        
+    C_dev.at(gid) = A_dev.at(gid)*B_dev.at(gid);
     
-    int gid = blockIdx.x*blockDim.x + threadIdx.x;
-
-    vecmem::device_vector< Acts::BoundSymMatrix > A_dev(A_view);
-    vecmem::device_vector< Acts::BoundSymMatrix > B_dev(B_view);
-    vecmem::device_vector< Acts::BoundSymMatrix > C_dev(C_view);
-
-    if (gid >= A_dev.size()){
-	return;
-    }
-        
-    C_dev.at(gid) = A_dev.at(gid)*B_dev.at(gid);
-}
-
-__global__ void matmul_kernel(
-    vecmem::data::vector_view< Acts::FreeSymMatrix > A_view,
-    vecmem::data::vector_view< Acts::FreeSymMatrix > B_view,
-    vecmem::data::vector_view< Acts::FreeSymMatrix > C_view){
-
-    int gid = blockIdx.x*blockDim.x + threadIdx.x;
-
-    vecmem::device_vector< Acts::FreeSymMatrix > A_dev(A_view);
-    vecmem::device_vector< Acts::FreeSymMatrix > B_dev(B_view);
-    vecmem::device_vector< Acts::FreeSymMatrix > C_dev(C_view);
-
-    if (gid >= A_dev.size()){
-	return;
-    }
-        
-    C_dev.at(gid) = A_dev.at(gid)*B_dev.at(gid);
 }
