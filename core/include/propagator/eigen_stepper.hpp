d16172cb4964e7c0fbe5a299bed6bbdd51838a96
beomki yeo <beomki.yeo@gmail.com>
2021-07-16 21:01:01 -0700

    minor addition

---
+++ core/include/propagator/eigen_stepper.hpp
@@ -0,0 +1,16 @@
+/** TRACCC library, part of the ACTS project (R&D line)
+ *
+ * (c) 2021 CERN for the benefit of the ACTS project
+ *
+ * Mozilla Public License Version 2.0
+ */
+
+namespace traccc{
+
+struct eigen_stepper{
+
+
+};
+    
+
+} //namespace traccc
