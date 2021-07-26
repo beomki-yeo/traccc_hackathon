725ce0b54cc6ab439ec64587a288340523ca4b05
Louis-Guillaume Gagnon <gagnonlg@protonmail.com>
2021-07-26 14:02:14 -0700

    First working cpu version (kalman actor)

--- core/include/fitter/kalman_fitter.hpp
+++ core/include/fitter/kalman_fitter.hpp
@@ -7,6 +7,7 @@
 
 #pragma once
 
+#include <edm/detail/transform_free_to_bound.hpp>
 #include <edm/measurement.hpp>
 #include <geometry/surface.hpp>
 
@@ -27,6 +28,12 @@ class kalman_fitter {
         // ID of target surface
         int target_surface_id;
 
+	// Ref to the measurements
+	traccc::host_measurement_collection& input_measurements;
+
+	// Ref to the surfaces
+	traccc::host_surface_collection& surfaces;
+
         /// Whether to consider multiple scattering.
         bool multiple_scattering = true;
 
@@ -36,12 +43,58 @@ class kalman_fitter {
         /// Whether run reversed filtering
         bool reversed_filtering = false;
 
-	// FIXME
-	traccc::host_measurement_collection* input_measurements;
+	actor(int target_surface_id,
+	      traccc::host_measurement_collection& input_measurements,
+	      traccc::host_surface_collection& surfaces,
+	      bool multiple_scattering = true,
+	      bool energy_loss = true,
+	      bool reversed_filtering = false) :
+	    target_surface_id(target_surface_id),
+	    input_measurements(input_measurements),
+	    surfaces(surfaces),
+	    multiple_scattering(multiple_scattering),
+	    energy_loss(energy_loss),
+	    reversed_filtering(reversed_filtering) {}
+
 
         template <typename propagator_state_t, typename stepper_t>
         void operator()(propagator_state_t& state,
                         const stepper_t& stepper) const {
+
+	    // Find matching measurement
+	    auto res = std::find_if(input_measurements.begin(), input_measurements.end(),
+			 [this] (traccc::measurement &m) {
+			     return m.surface_id == target_surface_id;
+			 }
+	    );
+	    if (res == input_measurements.end())
+		return;
+
+
+	    traccc::measurement &meas = *res;
+
+	    // Find the surface itself
+	    if (target_surface_id >= surfaces.items.size())
+		return;
+
+	    traccc::surface &surf = surfaces.items.at(target_surface_id);
+
+	    //state: measurement, predicted vector + covariance
+	    typename updater_t::track_state_type tr_state;
+	    tr_state.measurement() = meas;
+	    tr_state.predicted().covariance() = state.stepping.cov;
+	    tr_state.projector() = Acts::ActsMatrix<2,6>::Zero();
+	    tr_state.projector()(0,0) = 1;
+	    tr_state.projector()(1,1) = 1;
+
+	    tr_state.predicted().vector() =
+		traccc::detail::transform_free_to_bound_parameters(state.stepping.pars, surf);
+
+	    updater_t updater;
+	    updater(tr_state);
+
+	    state.stepping.pars = traccc::detail::transform_bound_to_free_parameters(tr_state.filtered().vector(), surf);
+	    state.stepping.cov = tr_state.filtered().covariance();
 	}
     };
 
--- tests/cuda/algorithms/actor_tests.cpp
+++ tests/cuda/algorithms/actor_tests.cpp
@@ -1,11 +1,13 @@
 #include <gtest/gtest.h>
 
+#include "csv/csv_io.hpp"
 #include <edm/measurement.hpp>
 #include <edm/track_parameters.hpp>
 #include <edm/track_state.hpp>
 #include <fitter/gain_matrix_smoother.hpp>
 #include <fitter/gain_matrix_updater.hpp>
 #include <fitter/kalman_fitter.hpp>
+#include "geometry/surface.hpp"
 #include <propagator/direct_navigator.hpp>
 #include <propagator/eigen_stepper.hpp>
 #include <propagator/propagator.hpp>
@@ -17,38 +19,62 @@ using propagator_t = traccc::propagator<stepper_t, navigator_t>;
 using propagator_options_t = traccc::propagator_options<traccc::void_actor, traccc::void_aborter>;
 using propagator_state_t = propagator_t::state<propagator_options_t>;
 using measurement_t = traccc::measurement;
-using parameters_t = traccc::free_track_parameters;
+using parameters_t = traccc::bound_track_parameters;
 using track_state_t = traccc::track_state<measurement_t, parameters_t>;
 using updater_t = traccc::gain_matrix_updater<track_state_t>;
 using smoother_t = traccc::gain_matrix_smoother<track_state_t>;
 using kalman_fitter_t = traccc::kalman_fitter<propagator_t, updater_t, smoother_t>;
 using actor_t = kalman_fitter_t::actor<parameters_t>;
 
-#include <edm/measurement.hpp>
+
+traccc::host_surface_collection read_surfaces()
+{
+	std::string full_name = std::string(__FILE__);
+	std::string dir = full_name.substr(0, full_name.find_last_of("\\/"));
+	std::string io_detector_file =
+		dir + std::string("/detector/trackml-detector.csv");
+
+	traccc::surface_reader sreader(
+		io_detector_file, {"geometry_id", "cx", "cy", "cz", "rot_xu", "rot_xv",
+					      "rot_xw", "rot_zu", "rot_zv", "rot_zw"});
+
+	auto surface_transforms = traccc::read_surfaces(sreader);
+
+	traccc::host_surface_collection surfaces;
+
+	for (auto tf : surface_transforms) {
+		traccc::surface surface(tf.second, tf.first);
+		surfaces.items.push_back(std::move(surface));
+	}
+
+	return surfaces;
+}
+
+
 
 TEST(algorithm, actor)
 {
+	traccc::host_surface_collection surfaces = read_surfaces();
+
 	measurement_t meas;
 	meas.local = {1, 1};
 	meas.variance = {1, 1};
-	meas.surface_id = 1;
+	meas.surface_id = 0;
 
 	traccc::host_measurement_collection meascoll = { meas };
 
 	stepper_t stepper;
 	propagator_state_t prop_state;
 
-	actor_t actor;
-	actor.target_surface_id = prop_state.navigation.current_surface_id;
-	actor.input_measurements = &meascoll;
+	actor_t actor(meas.surface_id, meascoll, surfaces);
 
 	Acts::FreeVector tpars_before = prop_state.stepping.pars;
 	actor(prop_state, stepper);
 	Acts::FreeVector tpars_after = prop_state.stepping.pars;
 
 	bool any_diff = false;
-	for (int i = 0; i < Acts::eFreeSize; i++)
-		any_diff |= (tpars_before != tpars_after);
+	for (int i = 0; i < Acts::eBoundSize; i++)
+		any_diff |= (tpars_before[i] != tpars_after[i]);
 
 	EXPECT_TRUE(any_diff);	
 }
